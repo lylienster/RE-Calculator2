ad51467e5793faefe99799f6494355a7e1111fb0
Tai Le <lylienster@gmail.com>
2020-04-12 17:51:47 -0400

    install react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="text-center">RE Calculator</h1>
+
+        <hr className="solid" />
+
+        <Button
+          variant="primary"
+          onClick={() => {}}
+          className="float-right"
+          style={{ marginRight: "10px" }}
+        >
+          Reset Numbers
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
